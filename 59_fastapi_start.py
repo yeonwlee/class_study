93805e28a86b477ed4c59e7be7fd6b35e47e9d43
yeonwlee <tlstprl1806@naver.com>
2024-09-10 17:23:20 +0900

    add: fastapi examples

--- 59_fastapi_start.py
+++ 59_fastapi_start.py
@@ -13,4 +13,4 @@ async def root():
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='localhost', port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=65535)
--- 60_fastapi_three_functions.py
+++ 60_fastapi_three_functions.py
@@ -45,42 +45,46 @@ class Chat(BaseModel):
     talk:str
     
     
-@app.post('/chatbot')
-async def chatbot(talk:str=Form(...)):
-    '''
-    이런 식으로 주석을 주면 redoc에서 이 내용이 보입니다.\n
-    이렇게 대충 달지 맙시다\n
-    챗봇과의 대화
-    '''
-    result = chat_chain.run(input=talk)
-    return {'result': result}
+# @app.post('/chatbot')
+# async def chatbot(talk:str=Form(...)):
+#     '''
+#     이런 식으로 주석을 주면 redoc에서 이 내용이 보입니다.\n
+#     이렇게 대충 달지 맙시다\n
+#     챗봇과의 대화
+#     '''
+#     result = chat_chain.run(input=talk)
+#     return {'result': result}
 
 
-@app.get('/readitem/{id}')# 이런 식으로 하면 뒤에 오는 값을 받을 수 있음
-async def readitem(id:str):
-    return id
+# @app.get('/readitem/{id}')# 이런 식으로 하면 뒤에 오는 값을 받을 수 있음
+# async def readitem(id:str):
+#     return id
 
 
-@app.post('/login')
-async def login(id:str=Form(...), pw:str=Form(...)):
-    return {'id':id, 'pw':pw}
+# @app.post('/login')
+# async def login(id:str=Form(...), pw:str=Form(...)):
+#     return {'id':id, 'pw':pw}
 
 
-@app.get('/upload')
-async def upload():
-    '''
-    이미지를 서버에 업로드함
-    '''
-    return {'result': '업로드 성공'}  
+# @app.get('/upload')
+# async def upload():
+#     '''
+#     이미지를 서버에 업로드함
+#     '''
+#     return {'result': '업로드 성공'}  
 
 
-@app.get('/aiimage')
-async def aiimage():
-    '''
-    이미지를 받아서 해당 이미지를 통해 연예인을 분류함
-    '''
-    return {'result': '카리나'}
+# @app.get('/aiimage')
+# async def aiimage():
+#     '''
+#     이미지를 받아서 해당 이미지를 통해 연예인을 분류함
+#     '''
+#     return {'result': '카리나'}
 
+
+@app.get('/')
+async def hello(): 
+    return 'hello'
 
 @app.post('/jsontest')
 async def jsontest(chatbot:Chat): # 위의 클래스 참고. 이렇게 해주면 데이터 타입이 맞는지 아닌지도 자동 체크해줍니다. ex. type 에 숫자 넣어보내면 오류남
@@ -88,4 +92,4 @@ async def jsontest(chatbot:Chat): # 위의 클래스 참고. 이렇게 해주면
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='localhost', port=7000)
+    uvicorn.run(app, host='0.0.0.0', port=65535)
